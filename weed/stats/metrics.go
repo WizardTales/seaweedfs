6c99bbec9cfaa76ea2322ec23dbbdb19f700c23e
Tobias Gurtzick <magic@wizardtales.com>
2026-01-28 00:49:07 +0100

    add necessary changes
    Signed-off-by: Tobias Gurtzick <magic@wizardtales.com>

--- weed/stats/metrics.go
+++ weed/stats/metrics.go
@@ -580,6 +580,30 @@ var (
 			Help:      "Counter of s3 server handlers.",
 		}, []string{"type"})
 
+	S3ReadCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: Namespace,
+			Subsystem: "s3",
+			Name:      "read_total",
+			Help:      "Counter of s3 server handlers.",
+		}, []string{"bucket"})
+
+	S3WriteCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: Namespace,
+			Subsystem: "s3",
+			Name:      "write_total",
+			Help:      "Counter of s3 server handlers.",
+		}, []string{"bucket"})
+
+	S3OtherCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: Namespace,
+			Subsystem: "s3",
+			Name:      "other_total",
+			Help:      "Counter of s3 server handlers.",
+		}, []string{"bucket"})
+
 	S3RequestHistogram = prometheus.NewHistogramVec(
 		prometheus.HistogramOpts{
 			Namespace: Namespace,
@@ -634,6 +658,14 @@ var (
 			Namespace: Namespace,
 			Subsystem: subsystemS3,
 			Name:      "bucket_traffic_sent_bytes_total",
+			Help:      "Total number of bytes sent from an S3 bucket to clients.",
+		}, []string{"bucket"})
+
+	S3BucketExternalSentBytesCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: Namespace,
+			Subsystem: "s3",
+			Name:      "bucket_traffic_sent_bytes_external_total",
 			Help:      "Total number of bytes sent from an S3 bucket to clients.",
 		}, []string{"bucket"})
 
@@ -956,6 +988,9 @@ func init() {
 	Gather.MustRegister(MasterVolumeCreationCounter)
 
 	Gather.MustRegister(S3RequestCounter)
+	Gather.MustRegister(S3ReadCounter)
+	Gather.MustRegister(S3OtherCounter)
+	Gather.MustRegister(S3WriteCounter)
 	Gather.MustRegister(S3HandlerCounter)
 	Gather.MustRegister(S3RequestHistogram)
 	Gather.MustRegister(S3InFlightRequestsGauge)
@@ -964,6 +999,7 @@ func init() {
 	Gather.MustRegister(S3TimeToFirstByteHistogram)
 	Gather.MustRegister(S3BucketTrafficReceivedBytesCounter)
 	Gather.MustRegister(S3BucketTrafficSentBytesCounter)
+	Gather.MustRegister(S3BucketExternalSentBytesCounter)
 	Gather.MustRegister(S3DeletedObjectsCounter)
 	Gather.MustRegister(S3UploadedObjectsCounter)
 	Gather.MustRegister(S3BucketSizeBytesGauge)
